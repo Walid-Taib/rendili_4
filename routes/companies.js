3f1105b8d46e21149b5d91bd3a56a4c07a00694a
walid taib <walidtaib44@gmail.com>
2023-02-21 23:23:35 +0100

    company router done

--- routes/companies.js
+++ routes/companies.js
@@ -2,23 +2,70 @@ const express=require('express');
 const { verifyUser, verifyCompany } = require('../authenticate');
 const Job = require('../models/job');
 const User = require('../models/user');
-const Jobrouter=express.Router();
+const CompanyRouter=express.Router();
 const cors=require('cors')
-Jobrouter.route('/')
+CompanyRouter.route('/')
 .get(cors() ,verifyUser,verifyCompany, (req,res,next)=>{
   User.findById(req.user._id)
+
   .then((resp)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type','application/json');
-    res.json(resp)
+
+      res.statusCode=200;
+      res.setHeader('Content-Type','application/json');
+      res.json(resp)
+    
+
+
 
   },(err)=>next(err))
   .catch((err)=>next(err))
 })
 
-.post(cors() ,verifyUser,(req,res,next)=>{
-  req.body.company=req.user
-  Job.create(req.body)
+
+
+.put((req,res,next)=>{
+  res.statusCode=404;
+  res.send('Operation is not available');
+}).post((req,res,next)=>{
+  res.statusCode=404;
+  res.send('Operation is not available');
+})
+
+.delete(cors(), verifyUser,verifyCompany, (req,res,next)=>{
+  User.remove()
+  .then((resp)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(resp)
+  })
+})
+
+
+CompanyRouter.route('/:companyId')
+.get(cors(),(req,res,next)=>{
+  User.findById(req.user._id)
+  .then((resp)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','application/json');
+    res.json(resp)
+  })
+})
+.post((req,res,next)=>{
+  res.statusCode=404;
+  res.send('Operation is not available');
+})
+.put(cors(), verifyUser,verifyCompany,(req,res,next)=>{
+  User.findByIdAndUpdate(req.user._id,{
+    $set:req.body
+  },{new:true})
+  .then((resp)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','application/json');
+    res.json(resp)
+  })
+})
+.delete(cors(),verifyUser,verifyCompany,(req,res,next)=>{
+  User.findByIdAndRemove(req.user._id)
   .then((resp)=>{
     res.statusCode=200;
     res.setHeader('Content-Type','application/json');
@@ -26,5 +73,5 @@ Jobrouter.route('/')
   })
 })
 
-module.exports=Jobrouter;
+module.exports=CompanyRouter;
 
